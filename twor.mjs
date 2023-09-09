c910623557ae1393d69213f3fab13003206ea8c4
Luís Möllmann <luism6n@gmail.com>
2023-09-08 23:38:54 -0300

    improve dx for running script

--- twor.mjs
+++ twor.mjs
@@ -1,8 +1,10 @@
 import fs from "fs/promises";
+import * as path from 'path';
 import slugify from "slugify";
 import { addColorSquares } from "./addSquaresToHtml.mjs";
 import { createSpecs } from "./prompt2spec.mjs";
 import { spec2config } from "./spec2config.mjs";
+import { writeFileSync } from "fs";
 
 function createSlug(title) {
   return slugify(title, {
@@ -11,8 +13,7 @@ function createSlug(title) {
   });
 }
 
-async function getPrompt() {
-  const filePath = process.argv[2];
+async function getPrompt(filePath) {
 
   if (!filePath || !filePath.endsWith(".txt")) {
     console.error("Por favor, forneça um arquivo com extensão .txt.");
@@ -47,10 +48,12 @@ const writeJson = async (jsonOutput) => {
 
 async function main() {
   try {
-    const prompt = await getPrompt();
+    const filePath = process.argv[2];
+    const prompt = await getPrompt(filePath);
     const jsonSpecs = await createSpecs(prompt);
     const formattedPalette = await spec2config(jsonSpecs);
-    console.log(formattedPalette);
+    const parsedFilePath = path.parse(filePath)
+    writeFileSync(`${parsedFilePath.name}.config.js`, JSON.stringify(formattedPalette));
     await addColorSquares(
       formattedPalette,
       prompt,
